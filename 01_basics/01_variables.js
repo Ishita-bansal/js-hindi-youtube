fd886eaf3c6b9eb4b4fdb296db33b815b04f1072
Ishita-bansal <87134060+Ishita-bansal@users.noreply.github.com>
2023-10-28 18:24:52 +0000

    let,const,var

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+//accountId = 2  // not allowed as const variables cannot change
+accountEmail = "hchc@gmail.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword , accountCity]);
