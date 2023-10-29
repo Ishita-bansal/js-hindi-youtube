f027275ff1b9853e7961220cdcc991a2b054da38
Ishita-bansal <87134060+Ishita-bansal@users.noreply.github.com>
2023-10-29 17:01:53 +0000

    dataType

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,16 @@
+"use strict";
+// alert(3+3)
+console.log(3+3);
+console.log("hitesh");
+console.log(typeof "hitesh");
+
+
+let name ="hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+console.log(typeof age);
+console.log(typeof null); // object
+console.log(typeof undefined);//undefined
+
+
