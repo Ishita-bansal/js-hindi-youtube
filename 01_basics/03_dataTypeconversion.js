9f865b4294df320cd34ebfd166371b8566d0c26f
Ishita-bansal <87134060+Ishita-bansal@users.noreply.github.com>
2023-11-03 12:56:13 +0000

    operations

--- 01_basics/03_dataTypeconversion.js
+++ 01_basics/03_dataTypeconversion.js
@@ -58,3 +58,46 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+// **************************** operaions *******************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);
+
+let str1 = "hello"
+let str2 = "hitesh"
+let str3 = str1 + str2 
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+
+console.log(true);
+console.log(+true);
+console.log(+"");
+
+let num1 , num2 , num3 ;
+num1 = num2 = num3 = 2+2
+
+let gamecounter = 100 
+gamecounter++
+console.log(gamecounter);
+
+// postfix
+let x = 3;
+const y = x++;
+console.log( x , y);
+
+// prefix
+let a = 3;
+const b = ++a ;
+console.log(a,b);
