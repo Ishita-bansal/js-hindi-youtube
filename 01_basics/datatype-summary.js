a013a7367bcaf5baff0ec868cb54165a08a7deee
Ishita-bansal <87134060+Ishita-bansal@users.noreply.github.com>
2023-11-03 17:15:24 +0000

    datatype-summary

---
+++ 01_basics/datatype-summary.js
@@ -0,0 +1,31 @@
+const score = 100
+const scorevalue = 100.3
+const isLoggedIn = false
+const outsidetemp = null
+let useremail;
+
+const id = Symbol('123');
+const anotherId = Symbol('123')
+console.log(id === anotherId);
+
+const bigNumber = 34567234537854326789334445555n
+
+// array
+const heros = ["shaktiman" , "neeraj" , "doga"];
+
+// object
+let myObj = {
+    name : "isha",
+    age : 21
+}
+
+// function
+const myFunction = function(){
+    console.log("Hello world");
+}
+console.log(typeof bigNumber);
+console.log(typeof outsidetemp);
+console.log(typeof myFunction);
+console.log(typeof myObj);
+console.log(typeof heros);
+console.log(anotherId);
