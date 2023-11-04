d3355e93e0af6e98f3a5271a1fa33d48eb566355
Ishita-bansal <87134060+Ishita-bansal@users.noreply.github.com>
2023-11-04 07:48:24 +0000

    string-methoeds

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -2,3 +2,27 @@ const name ="isha"
 const repocount = 50;
 // console.log(name + repocount + "value");
 console.log(`hello my name is ${name} and my repo count is ${repocount}`);
+
+// const gameName = new String("isha");
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('s'));
+
+const gameName = new String("isha-bansal");
+const newstring = gameName.substring(0,7);
+console.log(newstring);
+
+const anotherstring = gameName.substring(-8,4);
+console.log(anotherstring);
+
+const newstringOne = "      isha     "
+console.log(newstringOne);
+console.log(newstringOne.trim());
+
+const url = "https://www.google.com/isha%20bansal"
+console.log(url.replace('%20', '-'));
+console.log(url.includes('isha'));
+console.log(gameName.split('-'));
