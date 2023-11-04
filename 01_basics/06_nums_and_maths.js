1505dc244a68b183f0c5ca61df9f19bf9e2f60e4
Ishita-bansal <87134060+Ishita-bansal@users.noreply.github.com>
2023-11-04 14:20:51 +0000

    math-and-num

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,38 @@
+const score = 100 
+console.log(score);
+const balance = new Number(400)
+console.log(balance);
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber =  23.896;
+console.log(otherNumber.toPrecision(3));
+const otherNumber1 =  123.896;
+console.log(otherNumber1.toPrecision(3));
+
+const hundreds = 1000000;
+console.log(hundreds.toLocaleString('en-IN'));
+
+// +++++++++++++++++++++++ Maths +++++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.3));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+console.log(Math.floor(4.2));
+console.log(Math.sqrt(25));
+console.log(Math.pow(4,2));
+console.log(Math.max(5,4,3,8,9,10));
+console.log(Math.min(1,3,4,5,6));
+console.log(Math.random());
+console.log(Math.random()*10 + 1);
+const min = 10; 
+const max = 20;
+console.log(Math.floor(Math.random()* (max - min + 1)) + min); 
+
+
+
+
+
