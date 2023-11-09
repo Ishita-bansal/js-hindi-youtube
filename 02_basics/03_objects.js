267fd3b17f150969795004fa038b899eb16bd0de
Ishita-bansal <87134060+Ishita-bansal@users.noreply.github.com>
2023-11-09 11:27:53 +0000

    03_objects

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -9,7 +9,20 @@ const JsUser ={
     isLoggedIn : false,
     LastLofinDays:["mondays","satursday"]
 }
-console.log(JsUser.email);
-console.log(JsUser["email"]);
-console.log(JsUser["full name"]);
-console.log(JsUser[mySym]);
+// console.log(JsUser.email);
+// console.log(JsUser["email"]);
+// console.log(JsUser["full name"]);
+// console.log(JsUser[mySym]);
+JsUser.email="hitesh@chatgpt.com"
+// console.log(JsUser["email"]);
+// Object.freeze(JsUser)
+// JsUser.email="hitesh@microsoft.com"
+// console.log(JsUser);
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user,${this.name}`);
+}
+console.log(JsUser.greeting);
+console.log(JsUser.greetingTwo());
