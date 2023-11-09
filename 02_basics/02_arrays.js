bd84853b2c650fe1a2e4af873d43f60022b04617
Ishita-bansal <87134060+Ishita-bansal@users.noreply.github.com>
2023-11-09 12:59:06 +0530

    strings

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -5,3 +5,5 @@ marvel_heros.push(dc_heros);
 // console.log(marvel_heros[3][1]);
 marvel_heros.concat(dc_heros);
 console.log(marvel_heros);
+const allheros = marvel_heros.concat(dc_heros)
+console.log(allheros);
