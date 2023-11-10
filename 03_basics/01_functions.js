97b4a496b8f97fb748d10f69d5565a8044128f00
Ishita-bansal <87134060+Ishita-bansal@users.noreply.github.com>
2023-11-10 15:21:15 +0000

    01_functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -11,10 +11,26 @@ function saymyname() {
 
 // write function to add two numbers
 
+// function addtwonumbers(number1,number2){
+//     console.log(number1+number2);
+// }
+// addtwonumbers(5,10)
+// addtwonumbers(3,"4")
+// addtwonumbers(6,"a")
+// addtwonumbers(4,null)
 function addtwonumbers(number1,number2){
-    console.log(number1+number2);
+    // let result = number1+number2
+    // return result
+    return number1+number2
+     }
+const result = addtwonumbers(3,5)
+//console.log("result:", result);
+
+function loginuserMessage(username){
+    if(username === undefined){
+       console.log("Enter your username");
+    }
+    return`${username} just logged in`
 }
-addtwonumbers(5,10)
-addtwonumbers(3,"4")
-addtwonumbers(6,"a")
-addtwonumbers(4,null)
+//console.log(loginuserMessage("hitesh"));
+console.log(loginuserMessage());
