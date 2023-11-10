061a66127e19d708a7e61baa4083db713b24333f
Ishita-bansal <87134060+Ishita-bansal@users.noreply.github.com>
2023-11-10 13:27:10 +0000

    04_objects

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -3,7 +3,7 @@ const tinderUser = {}
 tinderUser.id = "123abc"
 tinderUser.name = "summy"
 tinderUser.isLoggedIn = false;
-console.log(tinderUser)
+// console.log(tinderUser)
 // objects contain further object
 const regularUser = {
     fullname : {
@@ -13,16 +13,16 @@ const regularUser = {
         }
     }
 }
-console.log(regularUser.fullname.username)  // { firstname: 'ishita', lastname: 'bansal' }
-console.log(regularUser.fullname.username.firstname) // ishita
+// console.log(regularUser.fullname.username)  // { firstname: 'ishita', lastname: 'bansal' }
+// console.log(regularUser.fullname.username.firstname) // ishita
 // array of objects
 const obj1 = {1:"a" , 2:"b"}
 const obj2 = {3:"a",4:"b"}
 const obj5 = {5:"a",6:"b"}
 const obj3 = {...obj1,...obj2}
-console.log(obj3);
-const obj4 = Object.assign({},obj1,obj2,obj5)
-console.log(obj4);
+// console.log(obj3);
+// const obj4 = Object.assign({},obj1,obj2,obj5)
+// console.log(obj4);
 
 // when values come for database
 
@@ -36,9 +36,39 @@ const users = [
         email:"i@gmail.com"
     }
 ]
-console.log(users[1].email);
-console.log(tinderUser);
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+// console.log(users[1].email);
+// console.log(tinderUser);
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+//destructuring the object
+const course = {
+    coursename : "JS in hindi",
+    price:"999",
+    courseInstructor:"hitesh"
+}
+// course.courseInstructor
+const {courseInstructor} = course
+console.log(courseInstructor);
+const {courseInstructor : instructor} = course
+console.log(instructor);
+
+// const navbar = () => {
+
+// }
+// navbar(company = "hitesh")
+
+// JSON Structure
+// {
+//     "name": "hitesh",
+//     "coursename":"js in hindi"
+//     "price":"free"
+// }
+
+// [
+//     {},
+//     {},
+//     {}
+// ]
