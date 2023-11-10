9dd01bd199f6c91af0b90513523122d5f01073f9
Ishita-bansal <87134060+Ishita-bansal@users.noreply.github.com>
2023-11-10 14:25:29 +0000

    01_functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,20 @@
+function saymyname() {
+    console.log("h");
+    console.log("i");
+    console.log("t");
+    console.log("e");
+    console.log("s");
+    console.log("h");
+}
+
+// saymyname()
+
+// write function to add two numbers
+
+function addtwonumbers(number1,number2){
+    console.log(number1+number2);
+}
+addtwonumbers(5,10)
+addtwonumbers(3,"4")
+addtwonumbers(6,"a")
+addtwonumbers(4,null)
