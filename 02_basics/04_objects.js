a02ca5aeee983e785ff114bd9a6012f553718052
Ishita-bansal <87134060+Ishita-bansal@users.noreply.github.com>
2023-11-10 10:40:28 +0000

    04_objects

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -4,7 +4,7 @@ tinderUser.id = "123abc"
 tinderUser.name = "summy"
 tinderUser.isLoggedIn = false;
 console.log(tinderUser)
-
+// objects contain further object
 const regularUser = {
     fullname : {
         username:{
@@ -15,3 +15,29 @@ const regularUser = {
 }
 console.log(regularUser.fullname.username)  // { firstname: 'ishita', lastname: 'bansal' }
 console.log(regularUser.fullname.username.firstname) // ishita
+// array of objects
+const obj1 = {1:"a" , 2:"b"}
+const obj2 = {3:"a",4:"b"}
+const obj5 = {5:"a",6:"b"}
+const obj3 = {...obj1,...obj2}
+console.log(obj3);
+const obj4 = Object.assign({},obj1,obj2,obj5)
+console.log(obj4);
+
+// when values come for database
+
+const users = [
+    {
+        id:1,
+        email:"h@gmail.com"
+    },
+    {
+        id:2,
+        email:"i@gmail.com"
+    }
+]
+console.log(users[1].email);
+console.log(tinderUser);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+
