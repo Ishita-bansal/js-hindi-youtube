50aa7ac73cc844bfaf2a6ee297a30025ba3ec59c
Ishita-bansal <87134060+Ishita-bansal@users.noreply.github.com>
2023-11-10 10:56:01 +0000

    04_objects

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -40,4 +40,5 @@ console.log(users[1].email);
 console.log(tinderUser);
 console.log(Object.keys(tinderUser));
 console.log(Object.values(tinderUser));
-
+console.log(Object.entries(tinderUser));
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
